615082207cd38fba6fbc6700e92e1d03c244d68e
Mohd Mozammil Khan <moz_rhce@yahoo.com>
2019-11-11 23:14:39 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('Helloooo GDG DevFest !!!\n');
+  res.send('Helloooo GDG DevFest! This is Moz testing review-app feature in azure pipeline. Pull request will be initiated and approved triggering azure pipeline to deploy merged code onto azure K8 service.\n');
 });
 
 export { server };
