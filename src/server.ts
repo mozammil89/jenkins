1ff10630585177164c08e4c5624cfa2a0a29a145
Mohd Mozammil Khan <moz_rhce@yahoo.com>
2020-01-21 03:23:10 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('Helloooo GDG DevFest!!! This is Moz, testing GCP.\n');
+  res.send('Helloooo GDG DevFest!!! This is Moz, testing GCP. This should trigger a fresh build.\n');
 });
 
 export { server };
